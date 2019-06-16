1a63bec564cf20ae3f278c8a087573da6f380b51
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-06-16 17:14:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,7 @@ def reverse_each_word(string)
   end
 secondarray.join
 end
+original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
